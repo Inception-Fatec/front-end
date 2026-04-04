5132f7400f5660da111a792aa5ac022dd45cd759
Lucas Araujo <lucas.allbuquerque1234@gmail.com>
2026-04-04 18:34:35 -0300

    fix: #SCRUM-26 normalize email input to lowercase before sign-in (#24)
    * fix: #SCRUM-26 redirect to dashboard after successful login
    
    * style: #scrum-26 update login page styles and responsivity for improved UI consistency
    
    * fix: #SCRUM-26 normalize email input to lowercase before sign-in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
 
         const result = await signIn('credentials', {
             redirect: false,
-            email,
+            email: email.toLocaleLowerCase(),
             password
         });
 
