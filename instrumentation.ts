a4882c70331945396aa4e5132d448c947cfd778c
LucasAraujo1016 <lucas.allbuquerque1234@gmail.com>
2026-04-27 08:13:57 -0300

    style: #scrum-43 execute prettier to code pattern

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,7 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     console.log("[Instrumentation] Iniciando receptor MQTT...");
-    
+
     const { startReceptor } = await import("./app/api/mqtt/subscriber");
     startReceptor();
   }
